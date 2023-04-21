14cb7817e396464a666c7fd4da3b76a00e6dc42b
lucasmillanm <70648632+lucasmillanm@users.noreply.github.com>
2023-04-21 16:05:35 +0200

    giga commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
+import {Component, OnInit} from '@angular/core';
+import {IonicModule} from '@ionic/angular';
+import {HttpClient} from '@angular/common/http';
+import {ImageService} from '../services/image.service';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +10,53 @@ import { IonicModule } from '@ionic/angular';
   standalone: true,
   imports: [IonicModule],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+  selectedFile: File = new File([], '');
+
+  constructor(
+    private http: HttpClient,
+    private imageService: ImageService,
+  ) {
+  }
+
+  ngOnInit() {
+
+  }
+
+  onFileSelected(event: Event) {
+    console.log(event);
+    if (event) {
+      const htmlEvent: HTMLInputElement = (event.target as HTMLInputElement);
+      console.log('test 1');
+      setTimeout(() =>
+        console.log('test 3', htmlEvent, htmlEvent.files)
+      );
+      console.log('test 2');
+      if (htmlEvent.files) {
+        console.log(htmlEvent.files);
+        const file: File = htmlEvent.files[0];
+        if (file) {
+          this.selectedFile = file;
+          console.log(this.selectedFile);
+        }
+      }
+    }
+  }
+
+  onUpLoad() {
+    if (this.selectedFile) {
+      console.log(this.selectedFile)
+      const formData: FormData = new FormData();
+      console.log(this.selectedFile.name)
+      formData.append('image', this.selectedFile, this.selectedFile.name);
+      console.log('formdata: ', formData.get('image'));
+      this.imageService.uploadImage(formData)
+       .subscribe(
+        () => console.log("hola"),
+        () => console.log("did not work")
+      )
+    }
+  }
+
+
 }
