33736d87be46ad8962f07572bc1759e210344446
lucasmillanm <70648632+lucasmillanm@users.noreply.github.com>
2023-05-10 12:59:43 +0200

    add id to model

--- src/app/models/form.ts
+++ src/app/models/form.ts
@@ -1,9 +1,11 @@
 export class Form {
+  id: number;
   fullName: string;
   age: string;
   imageName: string;
 
   constructor() {
+    this.id = 0;
     this.fullName = '';
     this.age = '';
     this.imageName = '';
